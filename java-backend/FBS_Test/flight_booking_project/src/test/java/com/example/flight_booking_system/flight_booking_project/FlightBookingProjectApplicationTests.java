b0927a0d2f2bdfd0f2ded14d69fbef9fe63bd9ef
Arsalan37 <93148890+Arsalan37@users.noreply.github.com>
2024-03-15 23:08:20 +0530

    Add files via upload

---
+++ java-backend/FBS_Test/flight_booking_project/src/test/java/com/example/flight_booking_system/flight_booking_project/FlightBookingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flight_booking_system.flight_booking_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
