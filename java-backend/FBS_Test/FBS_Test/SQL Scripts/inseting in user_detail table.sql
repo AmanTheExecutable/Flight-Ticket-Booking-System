bf2da2fe095288b8f092bcdc273040e2826de466
Md Tauseef Akhtar <63928008+mdtauseef123@users.noreply.github.com>
2024-03-20 11:55:43 +0530

    Delete java-backend directory

--- java-backend/FBS_Test/FBS_Test/SQL Scripts/inseting in user_detail table.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user_detail (name,email,password)
-VALUES('arsalan' , 'arsalan37@gmail.com' , '123');
--- java-backend/FBS_Test/FBS_Test/flight_booking_project/src/main/java/com/example/flight_booking_system/flight_booking_project/Entity/Bookings.java
+++
@@ -1,4 +0,0 @@
-package com.example.flight_booking_system.flight_booking_project.Entity;
-
-public class Bookings {
-}
