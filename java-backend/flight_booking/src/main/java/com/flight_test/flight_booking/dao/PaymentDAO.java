e1f5aa3421ba571364b47c352a07c894dcda0c79
Aman <kvaman543@gmail.com>
2024-03-20 12:46:43 +0530

    Deleted a folder

--- java-backend/flight_booking/src/main/java/com/flight_test/flight_booking/dao/PaymentDAO.java
+++
@@ -1,7 +0,0 @@
-package com.flight_test.flight_booking.dao;
-
-import com.flight_test.flight_booking.response.PaymentStatus;
-
-public interface PaymentDAO {
-    PaymentStatus makePayment(String paymentData, int amount);
-}
--- java-backend/flight_booking/src/main/java/com/flight_test/flight_booking/service/PaymentService.java
+++
@@ -1,7 +0,0 @@
-package com.flight_test.flight_booking.service;
-
-import com.flight_test.flight_booking.response.PaymentStatus;
-
-public interface PaymentService {
-    PaymentStatus makePayment(String paymentData, int amount);
-}
